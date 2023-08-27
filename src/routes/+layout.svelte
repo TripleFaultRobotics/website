f548ca0443d236acacba8f0f10736489a9f7f88c
voxal <aidenhohoho@gmail.com>
2023-08-26 23:08:59 -0700

    make navbar always on top

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,7 @@
 <svelte:document on:scroll={() => atTop = document.documentElement.scrollTop === 0} />
 
 <header>
-  <nav class={`transition-all border flex flex-row py-2 px-4 my-3 mx-4 rounded-lg gap-2 backdrop-blur-md fixed h-16 w-[calc(100%-2rem)] items-center ${atTop ? "border-stone-800/0" : "border-stone-600/75 bg-stone-700/75"}`}>
+  <nav class={`transition-all border flex flex-row py-2 px-4 my-3 mx-4 rounded-lg gap-2 backdrop-blur-md fixed h-16 w-[calc(100%-2rem)] items-center z-50 ${atTop ? "border-stone-800/0" : "border-stone-600/75 bg-stone-700/75"}`}>
     <a href="/"><img src="/logo.svg" class="w-12 h-12 hover:opacity-75"/></a>
     <ul class="font-mono">
       <li><a href="/team" class="hover:underline hover:opacity-75"><HoverDecryptText content="TEAM"/></a></li>
