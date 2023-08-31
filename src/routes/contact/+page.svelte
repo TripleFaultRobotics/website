7eb511a37e04992eff89c2c42fa91f2035dd7e92
fishstew <42249546+krill11@users.noreply.github.com>
2023-08-30 17:42:46 -0700

    Contact Page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,9 @@
-<h1>coming soon</h1>
+<script>
+    import Card from "$lib/components/Card.svelte"
+  </script>
+  
+  <h1 class="text-6xl font-extrabold mb-8">Contact and Media</h1>
+  <Card class="sm:w-96">
+    <h3 class="font-bold text-xl text-center">Email</h3>
+    <h4 class="font-medium text-stone-400 mb-2 text-center">kirill@lutsenko.com</h4>
+  </Card>
