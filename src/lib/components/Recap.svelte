f0a70fe1e839cea4da6315b915c7ac8118ec26df
voxal <aidenhohoho@gmail.com>
2023-11-21 23:59:20 -0800

    tooltips

--- src/lib/components/Recap.svelte
+++ src/lib/components/Recap.svelte
@@ -3,7 +3,23 @@
   import { base } from "$app/paths";
 
   const bonuses = {
-    "auto-purple-red": "Purpled pixel placed down on team prop"
+    "auto-purple": "Purple pixel placed down on mark with team prop",
+    "auto-purple": "Purple pixel placed down on mark with team prop",
+    "auto-yellow": "Yellow dropped on corresponding april tag.",
+    "auto-navigation": "Robot parked during auto",
+    
+    "teleop-mosaic": "A mosaic was formed.",
+    "teleop-set-line-1": "The first set line was reached",
+    "teleop-set-line-2": "The second set line was reached",
+    "teleop-set-line-3": "The third set line was reached",
+
+    "endgame-drone-1": "A drone landed in zone 1, providing 30 points",
+    "endgame-drone-2": "A drone landed in zone 2, providing 20 points",
+    "endgame-drone-3": "A drone landed in zone 3, providing 10 points",
+    "endgame-navigation": "Robot parked during Teleop",
+    "endgame-suspend": "Robot suspended off ground, rewarding 20 points",
+    
+    "backstage": "Number of pixels pushed into the backstage"
   }
 
   export let match;
@@ -28,19 +44,19 @@
         <!-- whitespace is messing something up... -->
         {#each match.red.bonuses.auto ?? [] as bonus}
           {#if bonus === "purple"}
-            <img src="{base}/csicons/auto-purple-red.svg" />
+            <img src="{base}/csicons/auto-purple-red.svg" title={bonuses["auto-purple"]} />
           {:else}
-            <img src="{base}/csicons/auto-{bonus}.svg" />
+            <img src="{base}/csicons/auto-{bonus}.svg" title={bonuses["auto-"+bonus]} />
           {/if}
         {/each}
         {#each match.red.bonuses.teleop ?? [] as bonus}
-          <img src="{base}/csicons/teleop-{bonus}.svg" />
+          <img src="{base}/csicons/teleop-{bonus}.svg" title={bonuses["teleop-"+bonus]} />
         {/each}
         {#each match.red.bonuses.endgame ?? [] as bonus}
-          <img src="{base}/csicons/endgame-{bonus}.svg" />
+          <img src="{base}/csicons/endgame-{bonus}.svg" title={bonuses["endgame-"+bonus]} />
         {/each}
         {#if match.red.backstage > 0}
-          <div class="text-xl [width:fit-content!important]"><img src="{base}/csicons/pixel-white.svg" class="w-8 inline"/> x {match.red.backstage}</div>
+          <div class="text-xl [width:fit-content!important]" title={bonuses["backstage"]}><img src="{base}/csicons/pixel-white.svg" class="w-8 inline"/> x {match.red.backstage}</div>
         {/if}
       </div>
       <h1 class="text-6xl font-extrabold text-center mt-4 bg-red-900/50 rounded-md py-2">{match.red.score}</h1>
@@ -65,19 +81,19 @@
       <div class="bg-slate-700/50 rounded p-4 mt-4 [&>*]:w-8 [&>*]:inline-block [&>*]:mr-2">
         {#each match.blue.bonuses.auto ?? [] as bonus}
           {#if bonus === "purple"}
-            <img src="{base}/csicons/auto-purple-blue.svg" />
+            <img src="{base}/csicons/auto-purple-blue.svg" title={bonuses["auto-purple"]}/>
           {:else}
-            <img src="{base}/csicons/auto-{bonus}.svg" />
+            <img src="{base}/csicons/auto-{bonus}.svg" title={bonuses["auto-"+bonus]}/>
           {/if}
         {/each}
         {#each match.blue.bonuses.teleop ?? [] as bonus}
-          <img src="{base}/csicons/teleop-{bonus}.svg" />
+          <img src="{base}/csicons/teleop-{bonus}.svg" title={bonuses["teleop-"+bonus]}/>
         {/each}
         {#each match.blue.bonuses.endgame ?? [] as bonus}
-          <img src="{base}/csicons/endgame-{bonus}.svg" />
+          <img src="{base}/csicons/endgame-{bonus}.svg" title={bonuses["endgame-"+bonus]}/>
         {/each}
         {#if match.blue.backstage > 0}
-          <div class="text-xl [width:fit-content!important]"><img src="{base}/csicons/pixel-white.svg" class="w-8 inline"/> x {match.blue.backstage}</div>
+          <div class="text-xl [width:fit-content!important]" title={bonuses["backstage"]}><img src="{base}/csicons/pixel-white.svg" class="w-8 inline"/> x {match.blue.backstage}</div>
         {/if}
       </div>
       <h1 class="text-6xl font-extrabold text-center mt-4 bg-blue-900/50 rounded-md py-2">{match.blue.score}</h1>
