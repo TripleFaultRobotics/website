ddff78ef010468800a9a163c08513ffe691c4e9d
voxal <aidenhohoho@gmail.com>
2024-01-07 19:07:07 -0800

    screw mobile no. 2

--- src/routes/resources/pixel-drop/+page.svelte
+++ src/routes/resources/pixel-drop/+page.svelte
@@ -16,8 +16,8 @@
 />
 
 <!-- <h1 class="mt-4 font-black text-center text-8xl">Pixel Drop</h1> -->
-<div class="w-full text-6xl h-[75vh] flex flex-col md:flex-row items-center mt-8 md:-mt-16">
-  <h2 class="h-fit md:basis-[150%]">
+<div class="w-full h-[75vh] flex flex-col md:flex-row items-center mt-8 md:-mt-16">
+  <h2 class="text-4xl md:text-6xl h-fit md:basis-[150%]">
     An elegant purple pixel deposit system from Triple Fault.
   </h2>
   <div class="relative top-0 z-10 w-full h-full">
@@ -39,8 +39,8 @@
     <iframe
       src="https://docs.google.com/forms/d/e/1FAIpQLSfWn7aVr_EJpcMz1p4qDnJXL_deoVLnAwSO36TqAlHkvfb-ow/viewform?embedded=true"
       title="Request Form"
-      width="640"
       height="800"
+      class="w-full h-[60rem] md:h-[50rem] md:w-[40rem]"
       frameborder="0"
       marginheight="0"
       marginwidth="0"
@@ -49,17 +49,17 @@
     </iframe>
   </div>
   <div class="flex flex-col h-full md:justify-between">
-    <div class="mb-16 text-5xl leading-normal">
+    <div class="mb-16 text-3xl leading-normal md:text-5xl">
       <h2 class="mb-4 text-6xl text-center">Use the Drop</h2>
       <a
         href="{base}/resources/pixel-drop/documentation"
-        class="text-[#F4E3FF] hover:bg-[#F4E3FF] hover:text-[#231124] font-mono"
+        class="text-[#F4E3FF] hover:bg-[#F4E3FF] hover:text-[#231124] font-mono block mb-3 md:mb-0"
       >
         <HoverDecryptText content="Documentation" />
-      </a><br />
+      </a>
       <a
         href="{base}/resources/pixel-drop/assembly"
-        class="text-[#F4E3FF] hover:bg-[#F4E3FF] hover:text-[#231124] font-mono"
+        class="text-[#F4E3FF] hover:bg-[#F4E3FF] hover:text-[#231124] block font-mono"
       >
         <HoverDecryptText content="Assembly Instructions" />
       </a>
@@ -67,15 +67,15 @@
     <div>
       <h2 class="mb-4 text-6xl text-center">Gallery</h2>
 
-      <div class="grid grid-cols-2 gap-4">
+      <div class="grid gap-4 md:grid-cols-2">
         <img src={dropWO} class="rounded-md" />
         <img src={dropW} class="rounded-md" />
       </div>
     </div>
   </div>
 </div>
-<div class="mt-16 text-5xl md:mx-32 md:text-6xl">
-  <h3 class="text-center ">Credits</h3>
+<div class="mt-16 text-4xl md:mx-32 md:text-6xl">
+  <h3 class="mb-4 text-6xl text-center md:mb-0">Credits</h3>
   <div class="flex justify-between">
     <span>Ryan Yuan</span><span class="text-neutral-300">Design</span>
   </div>
