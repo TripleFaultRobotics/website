6ff37c6d3be76cf07f53dd8b73ad9f86a4dd2fc2
voxal <aidenhohoho@gmail.com>
2024-01-11 10:24:23 -0800

    Updates

---
+++ _app/immutable/chunks/index.15c719b2.js
@@ -0,0 +1,1 @@
+const e=typeof window<"u"?window:typeof globalThis<"u"?globalThis:global;let s=null;const n=l=>s=l;export{e as g,s as l,n as s};
--- _app/immutable/chunks/index.80b6cc5a.js
+++
@@ -1,1 +0,0 @@
-let e=null;const l=s=>e=s;export{e as l,l as s};
